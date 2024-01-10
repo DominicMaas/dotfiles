fd940fb6964e1a2ac0e16ac86bbf351af1acf882
Dominic Maas <dominic.maas@live.com>
2024-01-11 12:42:49 +1300

    Arch

---
+++ backup.sh
@@ -0,0 +1,15 @@
+
+rm -r nvim/
+cp -r $HOME/.config/nvim/ nvim/
+
+rm -r alacritty/
+cp -r $HOME/.config/alacritty/ alacritty/
+
+rm -r fuzzel/
+cp -r $HOME/.config/fuzzel fuzzel/
+
+rm -r sway/
+cp -r $HOME/.config/sway sway/
+
+rm -r waybar/
+cp -r $HOME/.config/waybar waybar/
