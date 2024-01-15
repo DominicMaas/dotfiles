e52306216cc792f4ec288ae06b6ead3c4b46d7ba
Dominic Maas <dominic.maas@live.com>
2024-01-16 03:50:30 +1300

    extend load script

--- load.sh
+++ load.sh
@@ -1,1 +1,15 @@
+rm -r $HOME/.config/nvim
 cp -r nvim/ $HOME/.config/nvim/
+
+rm -r $HOME/.config/alacritty
+cp -r alacritty/ $HOME/.config/alacritty
+
+rm -r $HOME/.config/fuzzel
+cp -r fuzzel/ $HOME/.config/fuzzel
+
+rm -r $HOME/.config/sway
+cp -r sway/ $HOME/.config/sway
+
+rm -r $HOME/.config/waybar
+cp -r waybar/ $HOME/.config/waybar
+
